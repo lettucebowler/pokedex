8ba0a60aa62b8b1100abf406bcd6ec5fd6d387f4
Grant Montgomery <lettucebowler@gmail.com>
2023-12-02 02:13:37 -0600

    put the type border back to border-2 for outer color

--- apps/ui/src/lib/components/TypeBox.svelte
+++ apps/ui/src/lib/components/TypeBox.svelte
@@ -4,10 +4,10 @@
 </script>
 
 <div
-	class="rounded-[1.375rem] border-4 border-[var(--color-secondary-type-300,_var(--color-default-300,_black))] bg-[var(--color-primary-type-200,_var(--color-default-200,_black))] p-1"
+	class="rounded-[1.375rem] border-2 border-[var(--color-secondary-type-300,_var(--color-default-300,_black))] bg-[var(--color-primary-type-200,_var(--color-default-200,_black))] p-1"
 >
 	<div
-		class="rounded-xl bg-[var(--color-primary-type-100,_var(--color-default-100,_black))] p-1 {className}"
+		class="rounded-2xl bg-[var(--color-primary-type-100,_var(--color-default-100,_black))] p-2 {className}"
 	>
 		<slot />
 	</div>
